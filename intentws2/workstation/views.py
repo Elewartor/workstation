011822b69177262fc3d8ccbde7a19f3c92189616
Andrii <ele33war@gmail.com>
2020-06-11 10:44:27 -0400

    Search adding

--- intentws2/workstation/views.py
+++ intentws2/workstation/views.py
@@ -1,15 +1,37 @@
 from django.shortcuts import render,redirect
 from workstation.models import Unit
+from django.db.models import Q
 
 # Create your views here.
 
 def home_view(request):
+    context = {}
     user = request.user
     if not user.is_authenticated:
         return redirect('login')
     else:
-        context = {}
         units = Unit.objects.all()
-        context['units'] = units
+        
         context['user'] = user
+
+        query = ""
+        if request.GET:
+            query = request.GET.get("q")
+            context['query'] = str(query)
+            units = get_units_queryset(str(query))
+
+        context['units'] = units
+        
     return render(request, 'home.html', context)
+
+def get_units_queryset(query=None):
+    queryset = []
+    queries = query.split(" ")
+    for q in queries:
+        units = Unit.objects.filter(
+                Q(title__icontains=q) | 
+                Q(upc__icontains=q)
+            ).distinct()    
+        for unit in units:
+            queryset.append(unit)
+    return list(set(queryset))
