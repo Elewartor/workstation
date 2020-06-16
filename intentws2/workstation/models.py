3a3f575c57502723bfbd9970291e4d814330d617
Andrii <ele33war@gmail.com>
2020-06-16 14:58:42 -0400

    Bootstap and Unit details added

--- intentws2/workstation/models.py
+++ intentws2/workstation/models.py
@@ -8,6 +8,7 @@ class Unit(models.Model):
     upc                 = models.CharField(max_length=12, null=False, blank=False)
     part_number         = models.CharField(max_length=10, null=False, blank=False)
     image_url           = models.CharField(max_length=500, null=False, blank=False)
+    details             = models.TextField(max_length=9000, null=True)
     author              = models.ForeignKey(Account, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
