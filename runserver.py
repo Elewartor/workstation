2305cc385e0cf148b0e4ddb1b46747078dcb963f
Andrew <intentwarehouse@gmail.com>
2020-10-22 15:29:57 -0400

    db data update

--- runserver.py
+++ runserver.py
@@ -2,7 +2,7 @@ import os
 
 project_path = 'C:\\Users\\IntentRefurbish\\Desktop\\workstation\\workstation\\intentws2'
 virtualenv = 'itentws2'
-ip = '192.168.1.113:8000'
+ip = '192.168.1.89:8000'
 
 os.chdir(project_path)
 os.system('workon '+ virtualenv +' && python manage.py runserver ' + ip)
